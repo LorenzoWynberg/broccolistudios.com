00d895243cebc389416e752333533b2021371d96
Lorenzo Wynberg <dev.wynberg@gmail.com>
2026-01-15 10:48:58 -0600

    style: increase STUDIOS font size

--- components/logo.tsx
+++ components/logo.tsx
@@ -6,7 +6,7 @@ export function Logo({ className = '' }: LogoProps) {
   return (
     <span className={`inline-flex flex-col leading-none ${className}`}>
       <span className="text-lg font-black tracking-tight">BROCCOLI</span>
-      <span className="text-[0.65rem] font-medium tracking-[0.35em]">STUDIOS</span>
+      <span className="text-xs font-medium tracking-[0.25em]">STUDIOS</span>
     </span>
   );
 }
